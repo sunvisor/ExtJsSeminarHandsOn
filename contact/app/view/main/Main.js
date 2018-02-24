b3879974ffd9f41d7cc9bdb9902dd16c59dbae20
中村久司 <hisashi@sunvisor.net>
2018-02-24 17:44:47 +0900

    step01 add list

--- contact/app/view/main/Main.js
+++ contact/app/view/main/Main.js
@@ -3,29 +3,32 @@
  */
 Ext.define('Contact.view.main.Main', {
     extend: 'Ext.panel.Panel',
-    xtype: 'app-main',
+    xtype : 'app-main',
 
     requires: [
         'Contact.view.list.List',
         'Contact.view.main.MainController',
         'Contact.view.main.MainModel',
+        'Ext.layout.container.Card',
         'Ext.layout.container.VBox',
         'Ext.plugin.Viewport'
     ],
 
     controller: 'main',
-    viewModel: 'main',
+    viewModel : 'main',
+
+    layout: {
+        type : 'vbox',
+        align: 'stretch'
+    },
 
     items: [{
         html: '<h2>My Contact List</h2>',
-        cls: 'header'
+        cls : 'header'
     }, {
-        layout  : {
-            type    : 'vbox',
-            align   : 'stretch'
-        },
-        items: [{
+        layout: 'card',
+        items : [{
             xtype: 'list'
         }]
     }]
-});
+})
